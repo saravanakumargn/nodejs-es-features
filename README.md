6a1572e8bc007110ece1ecbb372938b2fe254ade
Saravanakumar <saravanakumargn@gmail.com>
2017-03-05 13:44:46 +0530

    update json

---
+++ README.md
@@ -0,0 +1,11 @@
+# js-features
+
+## Deploy your changes
+
+Make some changes to the code you just cloned and deploy them to Heroku using Git.
+
+```sh
+$ git add .
+$ git commit -am "make it better"
+$ git push heroku master
+```
--- index.js
+++ index.js
@@ -1,14 +1,52 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var jsonfile = require('jsonfile');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false
+})
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-  response.send('Hello');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+  response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+
+var updateDescFile = './data/update.json';
+
+app.route('/update')
+  .get(function (request, response) {
+    response.render('pages/update');
+  })
+  .post(urlencodedParser, function (request, response) {
+    update = {
+      v: new Date().getTime(),
+      desc: request.body.desc
+    }
+    // console.log(request.body.desc, update);
+    //response.end('Saved');
+    jsonfile.writeFile(updateDescFile, update, function (err) {
+      // console.error(err);
+      // response.end('Saved');
+      response.render('pages/update');
+    })
+  });
+
+app.get('/api/update', function (request, response) {
+  jsonfile.readFile(updateDescFile, function (err, obj) {
+    // console.dir(obj)
+    response.json(obj);
+  })
+  // response.json({ na: 'te' });
+})
+  ;
+
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
